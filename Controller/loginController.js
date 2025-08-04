0bb6494c99b3b04f3151e55067678c2bb9d5c83f
Syed Rana <7syedrana@gmail.com>
2025-08-04 20:22:22 +0600

    modified login controller

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -19,32 +19,37 @@ let login = async (req, res) => {
 
         if (exitingUser){
 
-             // ✅ Check if user is approved by admin
-            if (!exitingUser.isApproved) {
-                return res.status(400).send("Your account is pending admin approval.");
+          // ✅ Check if user is email verification
+          if (!exitingUser.isVerified) {
+              return res.status(400).send("Your account is pending Email Verification.");
+          } 
+
+            // ✅ Check if user is approved by admin
+          if (!exitingUser.isApproved) {
+              return res.status(400).send("Your account is pending admin approval.");
+          }
+
+          bcrypt.compare(password, exitingUser.password, function(err, result) {
+            if (result){
+
+                const token = jwt.sign({
+                    username: exitingUser.name,
+                    userid: exitingUser._id,
+                    role: exitingUser.role,
+                }, process.env.JWT_SECRET,{
+                    expiresIn: "1h"
+                });
+
+                res.status(200).json({
+                    "access_token": token,
+                    "message": "Login Successful",
+                    "role": exitingUser.role,
+                });
+
+            }else{
+                return res.status(400).send("Invalid credentials");
             }
-
-            bcrypt.compare(password, exitingUser.password, function(err, result) {
-                if (result){
-
-                    const token = jwt.sign({
-                        username: exitingUser.name,
-                        userid: exitingUser._id,
-                        role: exitingUser.role,
-                    }, process.env.JWT_SECRET,{
-                        expiresIn: "1h"
-                    });
-
-                    res.status(200).json({
-                        "access_token": token,
-                        "message": "Login Successful",
-                        "role": exitingUser.role,
-                    });
-
-                }else{
-                    return res.status(400).send("Invalid credentials");
-                }
-            });
+        });
         } else {
             return res.status(400).send("Invalid credentials");
         }
